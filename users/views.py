605f86b2b142644df5ed81058ec72ed70224983f
José Ángel García <chepeaicrag12@gmail.com>
2022-06-02 13:06:35 -0500

    Updated: CRUD user

--- users/views.py
+++ users/views.py
@@ -2,10 +2,11 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
-from rest_framework.parsers import MultiPartParser, FormParser, ParseError
+from rest_framework.parsers import MultiPartParser, FormParser
 
 from .models import User
 from .serializers import UserSerializer, UserListSerializer
+
 
 class CrearListarUser(APIView):
 
@@ -18,17 +19,14 @@ class CrearListarUser(APIView):
         return Response(serializer.data, status=HTTP_200_OK)
 
     def post(self, request):
-        print(request.data)
-        try:
-            file = request.data['image']
-        except KeyError:
-            file = None
+        request.data._mutable = True
+        file = request.data.get('image', None)
         request.data['image'] = file
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=HTTP_200_OK)
-        
+
 
 class ActualizarListarEliminarUserById(APIView):
 
@@ -39,20 +37,20 @@ class ActualizarListarEliminarUserById(APIView):
         serializer = UserListSerializer(users, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
 
-    def update(self, request):
+    def put(self, request, id):
         user = User.objects.filter(status_delete=False, id=id).first()
         if not user:
             return Response({'message': 'Usuario no encontrado'}, status=HTTP_400_BAD_REQUEST)
-        serializer = UserSerializer(data=request.data, instance=user, partial=True)
+        serializer = UserSerializer(
+            data=request.data, instance=user, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=HTTP_200_OK)
-        
-    def update(self, request):
+
+    def delete(self, request, id):
         user = User.objects.filter(status_delete=False, id=id).first()
         if not user:
             return Response({'message': 'Usuario no encontrado'}, status=HTTP_400_BAD_REQUEST)
         user.status_delete = True
         user.save()
         return Response({'message': 'Usuario eliminado satisfactoriamente'}, status=HTTP_200_OK)
-
